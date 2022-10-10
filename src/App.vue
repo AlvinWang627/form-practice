33ecb84f214c349a1356651ddea338bd94dfebb1
Alvin <AlvinWang627@gmail.com>
2022-10-11 00:38:10 +0800

    form practice

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <FormulateInput
+      type="file"
+      name="file"
+      label="Select your documents to upload"
+      help="Select one or more PDFs to upload"
+      validation="mime:application/pdf|required"
+      :validation-messages="{
+        mime: '檔案必須是pdf檔',
+        required: '此為必填欄位',
+      }"
+      multiple
+    /> -->
+    <div class="date-group">
+      <!-- <label for="startDate">startDate</label> -->
+      <div class="day">{{ translateDays }}</div>
+      <input type="date" name="startDate" class="date" v-model="date" />
+    </div>
+
+    <FormulateInput
+      type="date"
+      name="sample"
+      label="Sample date input"
+      placeholder="Sample date placeholder"
+      help="Sample date help text"
+      validation="required|after:2019-01-01"
+      :min="min"
+      max="2021-01-01"
+      error-behavior="live"
+    />
+
+    <!-- <FormulateInput
+      type="time"
+      name="sample"
+      label="Sample time input"
+      placeholder="Sample time placeholder"
+      help="Sample time help text"
+      validation="required"
+    /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// const currentDate = document.querySelector('.test');
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  //禮拜幾
+  //mindays
+  data() {
+    return {
+      date: '',
+      min: '',
+    };
+  },
+  methods: {
+    getDays() {},
+  },
+  computed: {
+    translateDays() {
+      let weekday = new Date(this.date).getDay();
+      if (weekday === 1) {
+        return '(一)';
+      } else {
+        return '(二)';
+      }
+    },
+  },
+  watch: {
+    date(newValue, oldValue) {
+      this.min = newValue;
+      console.log(newValue, oldValue);
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import '../node_modules/@braid/vue-formulate/themes/snow/snow.scss';
+// .formulate-file-add {
+//   background-color: red !important;
+// }
+// .formulate-input-upload-area {
+//   background-color: yellow;
+// }
+
+.date-group {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  .date {
+    width: 200px;
+  }
+  .day {
+    left: 86px;
+    position: absolute;
+    pointer-events: none;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueFormulate from '@braid/vue-formulate';
+// import { cn } from '@braid/vue-formulate-i18n';
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.use(VueFormulate, {
+  // plugins: [cn],
+  // locale: 'cn',
+});
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
